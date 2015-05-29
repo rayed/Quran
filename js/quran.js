17ef9c52987467d9cf0fe1251b74b9c5bfe9b8d0
Rayed Alrashed <rayed@rayed.com>
2015-05-29 18:15:16 +0300

    Add UTF-8 and clean HTML, fix bug in rendering segments

--- js/quran.js
+++ js/quran.js
@@ -63,8 +63,11 @@ function load_page(page) {
             $a.addClass('aya_link');
             for (var j=0; j < aya.segs.length ; j++) {
                 seg = aya.segs[j];
-                if (seg.w < 30) continue;
-                if (seg.x < 30) seg.x = 0;
+                if (seg.w !=0 && seg.w < 30) continue;
+                if (seg.x < 30) {
+                    seg.w += seg.x;
+                    seg.x = 0;
+                }
                 $d = $('<div>')
                 .css('top', seg.y+'px')
                 .css('left', seg.x+'px')
